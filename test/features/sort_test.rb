de0cb7c2fe863e1df648d4a84d238672522372a1
cclaiborne <claiborne206@gmail.com>
2014-03-09 15:40:40 -0700

    fix test syntax

--- test/features/sort_test.rb
+++ test/features/sort_test.rb
@@ -1,8 +1,8 @@
 require "mergesort"
 require "minitest/autorun"
 
-describe "An array of unsorted integers" do
-  it "Sorted by the mergesort algorithm" do
+describe "an array of unsorted integers" do
+  it "sorted by the mergesort algorithm" do
        unsorted = [5,2,3,4,0,1]
        sorted = Mergesort.new.sort(unsorted)
        assert_equal sorted, [0, 1, 2, 3, 4, 5]
