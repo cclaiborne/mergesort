6b472f9c64b96e2ea6220b43d3c81f54d3f68f06
cclaiborne <claiborne206@gmail.com>
2014-03-09 15:39:39 -0700

    add mergesort and tests

--- lib/Mergesort.rb
+++ lib/Mergesort.rb
@@ -1,10 +1,25 @@
-require 'date'
 class Mergesort
-  def initialize(year, month)
-
+  def initialize
   end
-  def getdate(day)
 
+  def sort(list)
+    return list if list.size <= 1
+    middle = list.size / 2
+    left = list[0,middle]
+    right = list[middle..-1]
+    merge(sort(left), sort(right))
+  end
+
+  def merge(left, right)
+    results = []
+    until left.empty? || right.empty?
+      if left.first <= right.first
+        results << left.shift
+      else
+        results << right.shift
+      end
+    end
+    results + left + right
   end
 end
 
